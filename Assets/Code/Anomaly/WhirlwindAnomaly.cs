84dd931f7faedf6e9ede0f30e9fa7dd8781e47ff
Kurt <rotorist1@gmail.com>
2017-07-03 01:51:08 -0500

    7-3

---
+++ Assets/Code/Anomaly/WhirlwindAnomaly.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhirlwindAnomaly : MonoBehaviour
+{
+
+	void Update () 
+	{
+		transform.RotateAround(transform.position, transform.up, 80 * Time.deltaTime);
+	}
+
+}
