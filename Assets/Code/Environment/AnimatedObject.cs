62588cdaa0504e7dc694a325381e89b253a97538
Kurt <rotorist1@gmail.com>
2017-08-03 19:08:06 -0500

    8-3

---
+++ Assets/Code/Environment/AnimatedObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedObject : MonoBehaviour 
+{
+	public AnimatedObjectType MovementType;
+	public float Speed;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch(MovementType)
+		{
+		case AnimatedObjectType.Spin:
+			transform.RotateAround(transform.position, transform.forward, Speed * Time.deltaTime);
+			break;
+		}
+	}
+}
+
+public enum AnimatedObjectType
+{
+	Backforth,
+	Swing,
+	Spin,
+}
